37ad29a051f7c92cb887280c36141b6b44ba873e
LucaPertosa <luca.pertosa@hotmail.it>
2023-03-08 14:19:02 +0100

    Add: ragionamento logico js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-alert('ciao')
+// CALCOLO PREZZO BIGLIETTO DEL TRENO
+
+// LOGICA
+
+// Chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero
+
+// Calcolo prezzo del biglietto in base ai chilometri
+
+// Calcolo sconto in base all'età
+
+// Applico sconto sul prezzo del biglietto
+
+// Preparo il messaggio con il prezzo
+
+// Output messaggio
