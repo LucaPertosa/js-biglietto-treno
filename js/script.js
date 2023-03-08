6410a2f1a76a523c6d3d4d6f1a879386b9e710be
LucaPertosa <luca.pertosa@hotmail.it>
2023-03-08 14:24:28 +0100

    Add: const conosciute

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 // CALCOLO PREZZO BIGLIETTO DEL TRENO
 
+/* prezzo biglietto euro/km */
+const priceKm = 0.21 ;  
+/* sconto 20% > 18 anni */
+const discountSmall = 0.20 ;
+/* sconto 40% < 65 anni */
+const discountBig = 0.40 ;
+
 // LOGICA
 
 // Chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero
