c02996b378fed603e326bd82c5422555a0d75c43
LucaPertosa <luca.pertosa@hotmail.it>
2023-03-08 15:51:16 +0100

    Complete: ragionamento logico + calcolo e output

--- js/script.js
+++ js/script.js
@@ -3,20 +3,39 @@
 /* prezzo biglietto euro/km */
 const priceKm = 0.21 ;  
 /* sconto 20% > 18 anni */
-const discountSmall = 0.20 ;
+const discount20 = 0.20 ;
 /* sconto 40% < 65 anni */
-const discountBig = 0.40 ;
+const discount40 = 0.40 ;
 
 // LOGICA
 
 // Chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero
+alert ('Ciao! inserisci i dati per sapere quanto costerà il tuo biglietto!')
+const userDistance = prompt('Quanti km devi percorrere?')
+const userAge = prompt('Quanti anni hai?')
+console.log (userDistance, userAge);
 
-// Calcolo prezzo del biglietto in base ai chilometri
+if (isNaN(userDistance)) {
+    document.getElementById('error').innerHTML = "Errore, inserisci i dati scrivendo solo numeri. Ricarica la pagina"
+} else if (isNaN(userAge)) {
+    document.getElementById('error').innerHTML = "Errore, inserisci i dati scrivendo solo numeri. Ricarica la pagina"
+} else {
+    // Calcolo prezzo del biglietto in base ai chilometri
+    const price = userDistance * priceKm
+    console.log(price);
+    // Calcolo sconto in base all'età e lo applico al prezzo
+    let finalPrice = '';
 
-// Calcolo sconto in base all'età
+    if (userAge < 18){
+        finalPrice = (price - (discount20 * price)).toFixed(2)
+    } else if (userAge >= 65) {
+        finalPrice = (price - (discount40 * price)).toFixed(2)
+    } else {
+        finalPrice = price.toFixed(2)
+    }
+    // Preparo il messaggio con il prezzo
+    console.log(finalPrice);
 
-// Applico sconto sul prezzo del biglietto
-
-// Preparo il messaggio con il prezzo
-
-// Output messaggio
+    // Output messaggio
+    document.getElementById('ticket-cost').innerHTML = `Grazie per aver utilizzato il calcolatore! Il prezzo del tuo biglietto è di € ${finalPrice} ` ; 
+}
